ef7f06a2d1054477fd9f1cfe032def33f357e200
Gabriel Vaz <gabriel.v.jesus7@aluno.senai.br>
2025-10-17 10:53:09 -0300

    Ajustar o arquivo no repositorio
    Mover os arquivos para a pasta raiz do repositório

---
+++ 12_atualizar.php
@@ -0,0 +1,5 @@
+<!-- Passar id via URL -->
+<!-- http://localhost/php-basicos/12_atualizar.php?id=1-->
+
+
+
---
+++ 9_conexao_bd.php
@@ -0,0 +1,12 @@
+
+<!-- Para criar o BD -->
+<!-- CREATE DATABASE exercicio; -->
+
+<!-- Para criar a Tabela -->
+<!-- CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+); -->
+
+
